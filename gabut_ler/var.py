23516f985b5b208eaaf52d29010e1d8ab8b690ec
SnakeXrnt <67412985+SnakeXrnt@users.noreply.github.com>
2022-07-27 19:59:12 +0700

    2022-07-27_19-59-10

--- gabut_ler/var.py
+++ gabut_ler/var.py
@@ -6,5 +6,5 @@ alamat = input('Lalu masukan alamat kamu disini : \n')
 hobi = input('Hobi kamu apa ? \n')
 
 os.system('cls')
-print('Hallo ')
+print('Hallo ,')
 print('Oke jadi saya akan konfirmasi data kamu : \n')
