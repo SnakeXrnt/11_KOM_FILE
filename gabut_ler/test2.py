fa75ba95ce66b9d825cfcde7d495a23dab9640a2
SnakeXrnt <67412985+SnakeXrnt@users.noreply.github.com>
2022-07-27 20:19:42 +0700

    2022-07-27_20-19-42

--- gabut_ler/test2.py
+++ gabut_ler/test2.py
@@ -2,7 +2,8 @@
 @author Ethan Bastian 11 Mipa 2
 @email ethanbastian11@gmail.com
 @create date 2022-07-27 20:19:31
-@modify date 2022-07-27 20:19:34
+@modify date 2022-07-27 20:19:42
 @desc [description]
 '''
 
+sgau
