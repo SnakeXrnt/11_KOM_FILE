2edd90bcc0d4fa03c68b1093fbe900601a2b3ff0
SnakeXrnt <67412985+SnakeXrnt@users.noreply.github.com>
2022-07-27 20:25:44 +0700

    2022-07-27_20-25-44

--- gabut_ler/test2.py
+++ gabut_ler/test2.py
@@ -1,6 +1,6 @@
 ####################################
-# Ethan Bastian 11 Mipa 2                         #
-# ethanbastian11@gmail.com                          # 
-# create date : 2022-07-27 20:25:19             #
+# Ethan Bastian 11 Mipa 2
+# ethanbastian11@gmail.com
+# create date : 2022-07-27 20:25:44
 ####################################
 
