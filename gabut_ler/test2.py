1924df003eb09af762722ee99f8e8e84b112cf5e
SnakeXrnt <67412985+SnakeXrnt@users.noreply.github.com>
2022-07-27 20:16:17 +0700

    2022-07-27_20-16-17

--- gabut_ler/test2.py
+++ gabut_ler/test2.py
@@ -5,3 +5,20 @@
  * @modify date 2022-07-27 20:16:01
  * @desc [description]
  */
+
+from os import *
+system("cls")
+print ('Halo, selamat datang di aplikasi unfaedah')
+nama = input('Coba masukan nama kamu disini : \n')
+usia = int(input('Lalu masukan usia kamu disini : \n'))
+alamat = input('Lalu masukan alamat kamu disini : \n')
+hobi = input('Hobi kamu apa ? \n')
+
+system('cls')
+print('Hallo ,' ,nama)
+print('Oke jadi saya akan konfirmasi data kamu : \n')
+print('Nama kamu adalah : ' ,nama)
+print('Usia kamu adalah : ' ,usia)
+print('Alamat kamu adalah : ' ,alamat)
+print('Hobi kamu adalah : ' ,hobi)
+input('Tekan enter untuk melanjutkan')
