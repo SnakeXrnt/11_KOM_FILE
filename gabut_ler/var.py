50f5b180a1f530c65092c53bf089a566af6ebc84
SnakeXrnt <67412985+SnakeXrnt@users.noreply.github.com>
2022-07-27 19:58:46 +0700

    2022-07-27_19-58-46

--- gabut_ler/var.py
+++ gabut_ler/var.py
@@ -1,7 +1,9 @@
+from os import *
 print ('Halo, selamat datang di aplikasi unfaedah')
 nama = input('Coba masukan nama kamu disini : \n')
 usia = int(input('Lalu masukan usia kamu disini : \n'))
 alamat = input('Lalu masukan alamat kamu disini : \n')
 hobi = input('Hobi kamu apa ? \n')
+
 
 print('Oke jadi saya akan konfirmasi data kamu : \n')
