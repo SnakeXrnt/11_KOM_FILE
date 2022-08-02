3f719583664b2d54d440885930e2112e9cd4ccd3
SnakeXrnt <67412985+SnakeXrnt@users.noreply.github.com>
2022-08-02 17:01:45 +0700

    2022-08-02_17-01-45

--- py_plus/PROJECT/run.py
+++ py_plus/PROJECT/run.py
@@ -1,26 +1,25 @@
-from msilib.schema import LockPermissions
-from flask import Flask , render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def index(): 
+	return render_template("index.html")
 
-
-texts = ["Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos pariatur harum architecto fugit sunt quasi voluptatem iusto placeat, repudiandae, minus cupiditate asperiores exercitationem totam saepe eos ut repellendus veniam! Voluptatibus!" , "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos pariatur harum architecto fugit sunt quasi voluptatem iusto placeat, repudiandae, minus cupiditate asperiores exercitationem totam saepe eos ut repellendus veniam! Voluptatibus!" , "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos pariatur harum architecto fugit sunt quasi voluptatem iusto placeat, repudiandae, minus cupiditate asperiores exercitationem totam saepe eos ut repellendus veniam! Voluptatibus!", "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos pariatur harum architecto fugit sunt quasi voluptatem iusto placeat, repudiandae, minus cupiditate asperiores exercitationem totam saepe eos ut repellendus veniam! Voluptatibus!","Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos pariatur harum architecto fugit sunt quasi voluptatem iusto placeat, repudiandae, minus cupiditate asperiores exercitationem totam saepe eos ut repellendus veniam! Voluptatibus!"]
+texts = ["Cupidatat elit ex magna magna eu duis deserunt excepteur aliqua dolor fugiat commodo.", "Irure id magna eiusmod ad amet. Exercitation ad laboris laborum tempor culpa dolore exercitation sunt do.", "Amet excepteur eu culpa reprehenderit dolor sunt ex ea laborum do anim eu minim incididunt."]
 
 @app.route("/one")
 def one():
-    return texts[0]
+	return texts[0]
 
 @app.route("/two")
 def two():
-    return texts[1]
+	return texts[1]
 
 @app.route("/three")
 def three():
-    return texts[2]
+	return texts[2]
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+	app.run(debug=True)
+
