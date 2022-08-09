b2bdb1b8af6f10ced1cb48e3acfb29014a858027
SnakeXrnt <67412985+SnakeXrnt@users.noreply.github.com>
2022-08-09 17:13:48 +0700

    2022-08-09_17-13-47

--- py_plus/PROJECT/run.py
+++ py_plus/PROJECT/run.py
@@ -12,7 +12,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = FALSE
 db = SQLAlchemy(app)
 
