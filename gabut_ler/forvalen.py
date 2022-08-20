5e55ae45b4b6fb32dfd1ee732220fc3da4695ba9
SnakeXrnt <67412985+SnakeXrnt@users.noreply.github.com>
2022-08-21 00:33:28 +0700

    2022-08-21_00-33-28

--- gabut_ler/forvalen.py
+++ gabut_ler/forvalen.py
@@ -1,1 +1,6 @@
-from tkinter import *
+####################################
+# Ethan Bastian 11 Mipa 2
+# ethanbastian11@gmail.com
+# create date : 2022-08-21 00:33:23
+####################################
+
